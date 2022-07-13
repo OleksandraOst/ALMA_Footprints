cc247c228ebe6d766fe8528915dc08d38dae9c83
Oleksandra Ostapenko <53404646+OleksandraOst@users.noreply.github.com>
2022-07-13 15:24:27 -0700

    Final version

--- Save_footprints.py
+++ Save_footprints.py
@@ -1,4 +1,4 @@
-#This program save footprints from csv table to txt file (as CARTA regions)
+#This program save footprints from csv table (from ALMA archive) to txt file (as DS9 regions)
 import pandas as pd
 
 def extract_footprints(csv_table,path,name):
@@ -7,31 +7,28 @@ def extract_footprints(csv_table,path,name):
     footprint = table.loc[:,"Footprint"] #use only "Footprint" column
     print(footprint)
 
-    #Writes a text about CARTA regions properties:
-    file_name_txt = path+name+'/ALMA/'+name+'.txt'
-    file_name_reg = path+name+'/ALMA/'+name+'.reg'
-    file_name_pixel = path+name+'/ALMA/'+name+'_pixel.txt'
+    #path to region file:
+    region_path = path+name+'/ALMA/'+name
 
-    #with open(file_name_txt, 'w') as f:
-        #f.write('# Region file format: DS9 \nICRS \n')
-        #f.write('# Region file format: DS9 CARTA 2.0.0 \nglobal color=#FFAE42 dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1 \nicrs \n')
+    #Writes a region file with CARTA regions properties:
+    file_name_reg = region_path+'.reg'  #region file in reg format; you can use .txt also
 
-
+    table["Footprint"] = table["Footprint"].str.replace(' ',',') #to have region coordinates separated by ',',
+    #in this case you can easily use it in pyregion plotting
 
     with open(file_name_reg, 'w') as f:
-        #f.write('# Region file format: DS9 \nICRS \n')
-        f.write('# Region file format: DS9 CARTA 2.0.0 \nglobal dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1 \nicrs \n')
+        #writes heading to region file
+        f.write('# Region file format: DS9 \nglobal dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1 \nicrs \n')
 
-        #Adds available footprints of the galaxy cluster:
+    #edits csv table in such way to have syntax of region file readable by DS9 and pyregion
     table["Footprint"] = table["Footprint"].str.replace('(','')
     table["Footprint"] = table["Footprint"].str.replace('UNION','')
     table["Footprint"] = table["Footprint"].str.replace('Union ICRS','')
-    table["Footprint"] = table["Footprint"].str.replace('  Polygon ICRS ','polygon(')
-    table["Footprint"] = table["Footprint"].str.replace(' Polygon ICRS ',' polygon(')
-    table["Footprint"] = table["Footprint"].str.replace('  Circle ICRS ','circle(')
-    table["Footprint"] = table["Footprint"].str.replace('  Ellipse ICRS ','ellipse(')
-
-
+    table["Footprint"] = table["Footprint"].str.replace(',,Polygon,ICRS,','polygon(')
+    table["Footprint"] = table["Footprint"].str.replace(',Polygon,ICRS,',' polygon(')
+    table["Footprint"] = table["Footprint"].str.replace(',,Circle,ICRS,','circle(')
+    table["Footprint"] = table["Footprint"].str.replace(',,Ellipse,ICRS,','ellipse(')
+    #make regions of same band to have same color
     table['Footprint'].loc[table['Band'] == 3] = table['Footprint'].astype(str) + '# color=#2EE6D6 width=2 text={Band 3}'
     table['Footprint'].loc[table['Band'] == 4] = table['Footprint'].astype(str) + '# color=#FFFF00 width=2 text={Band 4}'
     table['Footprint'].loc[table['Band'] == 5] = table['Footprint'].astype(str) + '# color=#000000 width=2 text={Band 5}'
@@ -40,21 +37,17 @@ def extract_footprints(csv_table,path,name):
     table['Footprint'].loc[table['Band'] == 8] = table['Footprint'].astype(str) + '# color=#2F5233 width=2 text={Band 8}'
     table['Footprint'].loc[table['Band'] == 9] = table['Footprint'].astype(str) + '# color=#FFA500 width=2 text={Band 9}'
 
-
-    #
-    # with open(file_name_txt, 'a') as f:
-    #     f.writelines('\n'.join(table["Footprint"]))
-    # print(file_name_txt)
-
+    #add changes from csv table to region file
     with open(file_name_reg, 'a') as f:
         f.writelines('\n'.join(table["Footprint"]))
     print(file_name_reg)
 
 
-
-
 if __name__ == '__main__':
+    #path to file
     path = '/Users/ostapiko/Desktop/Master_project/ALMA_Lensing_Clusters/Archive/'
-    name = 'Abell 2744'
-    csv_table = path+name+'/ALMA/Selected_footprints.csv'
+    #name of the object
+    name = 'ACTCLJ0102-49151'
+    #name of csv table
+    csv_table = path+name+'/ALMA/All_footprints.csv'
     extract_footprints(csv_table,path,name)
